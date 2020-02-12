069f0ddaa481766a123013dfc7aa3381b711a7d1
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-12 19:56:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -102,7 +102,7 @@ def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart)
   final_cart = apply_clearance(couponed_cart)
-
+  
   total = 0
 
   i = 0
